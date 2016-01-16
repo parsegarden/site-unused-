da6f375a8c85d9784336f47feeb0a3e9c88a1580
sachinnarula <athletesachin.narula@gmail.com>
2016-01-16 10:17:35 +0530

    Updated Analyze page

--- public/assets/js/functions.js
+++ public/assets/js/functions.js
@@ -35,8 +35,8 @@ function analyzeCompetition() {
 $(function() {
    $('.mobile-nav-toggle').on('click', function(){
     var status = $(this).hasClass('is-open');
-    if(status){ $('.mobile-nav-toggle, .mobile-nav').removeClass('is-open'); }
-    else { $('.mobile-nav-toggle, .mobile-nav').addClass('is-open'); }
+    if(status){ $('.mobile-nav-toggle, .mobile-nav , body').removeClass('is-open'); }
+    else { $('.mobile-nav-toggle, .mobile-nav, body').addClass('is-open'); }
   });
 
 });
