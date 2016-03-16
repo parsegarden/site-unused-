b30f2dea68dc10a18aaab4b46179f825e6b7ffc0
sachinnarula <athletesachin.narula@gmail.com>
2016-03-16 15:41:04 +0530

    [Update][Add] posts and added jquery

--- public/assets/js/functions.js
+++ public/assets/js/functions.js
@@ -5,3 +5,13 @@ $(".container").click(function(){
 });
 
 });
+
+$(window).scroll(function () {
+  if ( $(this).scrollTop() > 200 && !$('header').hasClass('open') ) {
+    $('header').addClass('open');
+    $('header').slideDown();
+   } else if ( $(this).scrollTop() <= 200 ) {
+    $('header').removeClass('open');
+    $('header').slideUp();
+  }
+});
