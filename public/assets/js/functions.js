cd0c8e6424a9e0459965588003e577b28be5fc84
sachinnarula <athletesachin.narula@gmail.com>
2016-03-25 16:42:39 +0530

    [Added] fixed bar(top and bottom)

--- public/assets/js/functions.js
+++ public/assets/js/functions.js
@@ -12,19 +12,36 @@ $(".container").click(function(){
 $(window).scroll(function () {
   if ( $(this).scrollTop() > 200 && !$('.fixed-header').hasClass('open') ) {
     $('.fixed-header').addClass('open');
-    $('.fixed-header').slideDown();
+    $('.fixed-header').slideDown(100);
    } else if ( $(this).scrollTop() <= 200 ) {
     $('.fixed-header').removeClass('open');
-    $('.fixed-header').slideUp();
+    $('.fixed-header').slideUp(100);
   }
 });
 
 
 
 
+
+//Scroll bottom
+
+$(window).scroll(function () {
+  if ( $(this).scrollTop() > 200 && !$('.container-fixed-on').hasClass('float-down') ) {
+    $('.container-fixed-on').addClass('float-down');
+    $('.container-fixed-on').slideup(100);
+   } else if ( $(this).scrollTop() <= 200 ) {
+    $('.container-fixed-on').removeClass('float-down');
+    $('.container-fixed-on').slideDown(100);
+  }
+});
+
+
+
+
+
 //Expand search
 $(document).ready(function(){
- 
+
     $("#label").click(function(){
         $("input").addClass("focus");
     });
