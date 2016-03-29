7974e7c029bce663c3393a59055c0cef3ce93caa
sachinnarula <athletesachin.narula@gmail.com>
2016-03-29 08:44:20 +0530

    [Add] fixed social links

--- public/assets/js/functions.js
+++ public/assets/js/functions.js
@@ -7,34 +7,6 @@ $(".container").click(function(){
 });
 
 
-//Scroll header
-
-$(window).scroll(function () {
-  if ( $(this).scrollTop() > 200 && !$('.fixed-header').hasClass('open') ) {
-    $('.fixed-header').addClass('open');
-    $('.fixed-header').slideDown(100);
-   } else if ( $(this).scrollTop() <= 200 ) {
-    $('.fixed-header').removeClass('open');
-    $('.fixed-header').slideUp(100);
-  }
-});
-
-
-
-
-
-//Scroll bottom
-
-$(window).scroll(function () {
-  if ( $(this).scrollTop() > 200 && !$('.container-fixed-on').hasClass('float-down') ) {
-    $('.container-fixed-on').addClass('float-down');
-    $('.container-fixed-on').slideup(100);
-   } else if ( $(this).scrollTop() <= 200 ) {
-    $('.container-fixed-on').removeClass('float-down');
-    $('.container-fixed-on').slideDown(100);
-  }
-});
-
 
 
 
@@ -68,4 +40,46 @@ function mobileNav() {
     if(status){ $('.menu-link, .nav-main').removeClass('is-open'); }
     else { $('.menu-link, .nav-main').addClass('is-open'); }
   });
+}
+
+
+
+
+// Hide Header on on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('.post-action').outerHeight();
+
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('.post-action').removeClass('nav-down').addClass('nav-up');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('.post-action').removeClass('nav-up').addClass('nav-down');
+        }
+    }
+
+    lastScrollTop = st;
 }
